155fe814334a8a8df66d25c8ba6b3ed6f43e0437
Satvic Bajpai <satvicbajpai@gmail.com>
2025-04-22 01:16:08 +0530

    add app.log and custom loggers

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import logger from "./logger.js";
+import morgan from "morgan";
 import "dotenv/config";
 import express from "express";
 
@@ -7,11 +9,31 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 let teaData = [];
 let nextId = 1;
 
 //Add a New Tea
 app.post("/teas", (req, res) => {
+  logger.warn("Add a new tea");
+
   const { name, price } = req.body;
 
   const newTea = {
